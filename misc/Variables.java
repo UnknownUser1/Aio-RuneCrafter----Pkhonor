c33b4aee9f5d05d84094c51613cf250b9fd6241d
UnknownUser1 <ethanspeeder1@hotmail.com>
2015-03-24 12:20:08 +1100

    Update Variables.java

--- misc/Variables.java
+++ misc/Variables.java
@@ -1,6 +1,10 @@
 package aiorunecrafterpkhonor.misc;
 
+import java.text.DecimalFormat;
+
 import org.rev317.min.api.methods.SceneObjects;
+
+import aiorunecrafterpkhonor.Main;
 
 public class Variables {
 
@@ -12,5 +16,30 @@ public class Variables {
 
 	public static boolean toRelog() {
 		return Gui.checkBox_reLogin.isSelected();
+	}
+
+	public static int getHourlyRate(int variable) {
+		return (int) (((double) (variable - 0) * 3600000D) / (double) (System
+				.currentTimeMillis() - Main.startTime));
+	}
+
+	public static String addDecimals(int item) {
+		DecimalFormat form = new DecimalFormat("#,###");
+		return "" + form.format(item);
+
+	}
+
+	public static String runTime(long i) {
+
+		DecimalFormat nf = new DecimalFormat("00");
+		long millis = System.currentTimeMillis() - i;
+		long hours = millis / (1000 * 60 * 60);
+		millis -= hours * (1000 * 60 * 60);
+		long minutes = millis / (1000 * 60);
+		millis -= minutes * (1000 * 60);
+		long seconds = millis / 1000;
+
+		return nf.format(hours) + ":" + nf.format(minutes) + ":"
+				+ nf.format(seconds);
 	}
 }
